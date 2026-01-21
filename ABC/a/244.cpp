7d479965ab2f199a4892f201d3d0baeb43c8685a
arutako <karikoji2525@gmail.com>
2026-01-21 22:44:29 +0900

    w

---
+++ ABC/a/244.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    cout << s[n-1] << endl;
+}
---
+++ ABC/a/281.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = n; i >= 0; i--) {
+        cout << i << endl;
+    }
+}
